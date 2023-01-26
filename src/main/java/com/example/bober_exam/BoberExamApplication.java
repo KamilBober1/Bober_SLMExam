c63562fb75a022fd642d8df6980d1a9b3ac9b8c0
KaM1los <bober.k03@htlwienwest.at>
2023-01-26 18:06:03 +0100

    Initial commit

---
+++ src/main/java/com/example/bober_exam/BoberExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.bober_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoberExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoberExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bober_exam/BoberExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bober_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoberExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
